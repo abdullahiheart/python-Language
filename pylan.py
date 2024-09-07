1bd0230ead5ae1e70ed2fc52f3de579ff5ed5f62
abdullahiheart <abdalaheart@gmail.com>
2024-09-07 07:58:45 +0300

    variables

--- pylan.py
+++ pylan.py
@@ -1,6 +1,13 @@
-#string
-print('Abdull')
-print(20)
-print(3.4)
-print(True)
-print(False)
+# #string
+# print('Abdull')
+# print(20)
+# print(3.4)
+# print(True)
+# print(False)
+ 
+ #Variabls
+my = 'Abdull'
+age =21
+birth =2004
+_isfinished = True
+print(my,age,birth,_isfinished)
