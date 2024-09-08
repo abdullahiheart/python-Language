eb66d4eb74255392c36a831bc55b3bd762b56e89
abdullahiheart <abdalaheart@gmail.com>
2024-09-08 15:24:43 +0300

    inside variabls

--- pylan.py
+++ pylan.py
@@ -11,3 +11,12 @@ age =21
 birth =2004
 _isfinished = True
 print(my,age,birth,_isfinished)
+
+# Variabls sidee program kada dhaxdiisa logu isticmalaa
+name = 'abdull'
+age = 21
+print(name+ "Heart")
+print(name, "waxaa jiraa" ,age, "sano")
+print(name+ "I'm from somalia")
+
+ 
