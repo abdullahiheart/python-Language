351d4e708b4ddba99950063b046508d2c13c2253
abdullahiheart <abdalaheart@gmail.com>
2024-09-09 06:24:52 +0300

    add

--- pylan.py
+++ pylan.py
@@ -6,17 +6,20 @@
 # print(False)
  
  #Variabls
-my = 'Abdull'
-age =21
-birth =2004
-_isfinished = True
-print(my,age,birth,_isfinished)
+# my = 'Abdull'
+# age =21
+# birth =2004
+# _isfinished = True
+# print(my,age,birth,_isfinished)
 
 # Variabls sidee program kada dhaxdiisa logu isticmalaa
-name = 'abdull'
-age = 21
-print(name+ "Heart")
-print(name, "waxaa jiraa" ,age, "sano")
-print(name+ "I'm from somalia")
+# name = 'abdull'
+# age = 21
+# print(name+ "Heart")
+# print(name, "waxaa jiraa" ,age, "sano")
+# print(name+ "I'm from somalia")
 
+# sida loo sodawaco 3 """""" kaliya qoraal dheer lagu sodawaca
+print('''waxaan ahy arday dhigta jamacada
+      waxaan bartaa Computer Sceinse''')
  
